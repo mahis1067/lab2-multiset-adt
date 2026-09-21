3b2a3758cd628e15dec3ec8cd38d5dc268a2d81c
mahis1067 <mahi.shah1067@gmail.com>
2026-09-21 14:46:50 -0400

    LinkedListMultiset done

--- src/LinkedListMultiSet.java
+++ src/LinkedListMultiSet.java
@@ -26,22 +26,54 @@ public class LinkedListMultiSet extends MultiSet {
 
     public void remove(int item) {
 
+        Node curr = this.front;
+        Node prev = null;
+        while (curr != null) {
+            if (curr.item == item){
+                this.size -= 1;
+                if (prev != null)
+                    prev.next = curr.next;
+                else
+                    this.front = curr.next;
+                return;
+            }
+
+            Node temp = curr;
+            prev = curr;
+            curr = temp.next;
+        }
+
+        return;
+
     }
 
     public boolean contains(int item) {
+        Node curr = this.front;
+        while (curr != null){
+            if (curr.item == item)
+                return true;
+            curr = curr.next;
+        }
         return false;
     }
 
     public boolean isEmpty() {
-        return false;
+        return this.front == null;
     }
 
 
     public int count(int item) {
-        return -1;
+        int num_seen = 0;
+        Node curr = this.front;
+        while (curr != null){
+            if (curr.item == item)
+                num_seen += 1;
+            curr = curr.next;
+        }
+        return num_seen;
     }
 
     public int size() {
-        return -1;
+        return this.size;
     }
 }
